325bdd094d88126381b182fb3ca2af26e866058f
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-06-03 22:06:49 +0100

    I am on the phone

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,3 @@
 __version__ = "0.0.1"
 
-class objectRegister(object):
-    def __init__(self):
-        self.objects = {}
-    
-    def register(self, obj, name : str = None):
-        if name is None:
-            name = obj.__name__
-        self.objects[name] = obj
-    
-    def __get__(self, name : str):
-        return self.objects[name]
-
         
