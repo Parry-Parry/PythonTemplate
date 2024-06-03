24ba3e8e2b4a766932c51f2ca4ed1800dfc39508
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-06-03 21:50:15 +0100

    L

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
 __version__ = "0.0.1"
+
+class functionRegister(object):
+    def __init__(self):
+        self.functions = {}
+    
+    def register(self, func : callable, name : str = None):
+        if name is None:
+            name = func.__name__
+        self.functions[name] = func
+    
+    def __get__(self, name : str):
+        return self.functions[name]
+        
