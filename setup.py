c3ff82c1902d302f1f8283560d52d8fb1d81ed78
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-06-04 12:26:57 +0100

    ehehe

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name=package_name,  # The name of your package
     version='0.1.0',  # Your package version
     packages=find_packages(where='src'),  # Look for packages in the 'src' directory
-    package_dir={package_name: 'src'},  # Map the package name to the 'src' directory
+    package_dir={'': 'src'},  # Map the package name to the 'src' directory
     author='Anon A. Mous',
     author_email='anon@mous.com',
     description='A default python template',
